03aeca4e8adbf1bb6cf3decb682a5ff9a605b390
black99hard <yasser99hard>
2023-08-11 13:11:44 +0100

    dd

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
+from magic_calculation_102 import add, sub
 
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
-    from magic_calculation_102 import add, sub
+    """
+    Match bytecode provided by Holberton School.
 
+    This function performs a specific computation pattern based on the values of a and b.
+    """
     if a < b:
-        c = add(a, b)
+        result = add(a, b)
         for i in range(4, 6):
-            c = add(c, i)
-        return (c)
-
+            result = add(result, i)
+        return result
     else:
-        return(sub(a, b))
+        return sub(a, b)
